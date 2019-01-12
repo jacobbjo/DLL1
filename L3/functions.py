626311f8c0d575158feec2a646efba7d71e89cfe
jacobbjo <jacob.bjorkman@live.com>
2019-01-12 17:21:58 +0100

    L3 Started

---
+++ L3/functions.py
@@ -0,0 +1,284 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def readfile(file):
+    file_dict = unpickle(file)
+    data = file_dict[b"data"]
+    labels = file_dict[b"labels"]
+
+    # Generate one hot representation of the labels:
+    one_hots = np.zeros((10, len(labels)))
+
+    for ind, label in enumerate(labels):
+        one_hots[label, ind] = 1
+
+    return np.transpose(data/255), one_hots, np.array(labels)
+
+
+def save_mats(mat_list, name, folder):
+    for i in range(len(mat_list)):
+        file_path = folder + "/" + name + "_" + str(i)
+        np.savetxt(file_path, mat_list[i])
+
+
+def load_mats(name, folder):
+    mat_list = []
+
+    i = 0
+    while True:
+        file_path_str = folder + "/" + name + "_" + str(i)
+        file_path = Path(file_path_str)
+
+        if not file_path.is_file():
+            print("load_mats:  \"" + file_path_str + "\" is not found, breaking.")
+            break
+
+        mat = np.loadtxt(file_path_str)
+        if mat.ndim < 2:
+            # fixing shape of vectors
+            #print("fixing mat shape for: " + name + "_" + str(i))
+            mat = mat.reshape(-1, 1)
+
+        mat_list.append(mat)
+
+        i += 1
+
+    return mat_list
+
+
+def get_parameters(dim_img, am_labels, am_nodes, seed=-1):
+    W = []
+    b = []
+
+    loop_nodes = am_nodes[:]
+    loop_nodes.insert(0, dim_img)
+    loop_nodes.append(am_labels)
+
+    if seed > 0:
+        np.random.seed(seed)
+
+    for i in range(len(loop_nodes)-1):
+        # Xavier initialization from lecture notes
+        W.append(np.random.normal(0, (1/np.sqrt(dim_img)), (loop_nodes[i+1], loop_nodes[i])))
+        b.append(np.zeros((loop_nodes[i+1], 1)))
+
+    return [W, b]
+
+
+def get_parameters_he(dim_img, am_labels, am_nodes, seed=-1):
+    W = []
+    b = []
+
+    loop_nodes = am_nodes[:]
+    loop_nodes.insert(0, dim_img)
+    loop_nodes.append(am_labels)
+
+    if seed > 0:
+        np.random.seed(seed)
+
+    for i in range(len(loop_nodes) - 1):
+        W.append(np.random.normal(0, np.sqrt(2 / loop_nodes[i]), (loop_nodes[i+1], loop_nodes[i])))
+        b.append(np.zeros((loop_nodes[i+1], 1)))
+
+    return [W, b]
+
+
+def softmax(s):
+    return np.exp(s)/np.sum(np.exp(s), axis=0)
+
+
+def evaluate_classifier(X, W, b):
+    activations = [X]
+
+    for i in range(len(W)):
+        s = np.matmul(W[i], activations[-1]) + b[i]
+        x = np.maximum(0, s)
+
+        activations.append(x)
+
+    P = softmax(s)
+    return [P, activations]
+
+
+def compute_cost(X, Y, W, b, lamb):
+    """the sum of the loss of the network’s predictions for the images in X relative to
+    the ground truth labels and the regularization term on W"""
+
+    P = evaluate_classifier(X, W, b)[0]
+    YP = Y*P
+    YP_log_sum = 0
+
+    for r in range(YP.shape[0]):
+        for c in range(YP.shape[1]):
+            if YP[r, c] != 0:
+                YP_log_sum += np.log(YP[r, c])
+
+    cross = -1/X.shape[1] * YP_log_sum
+    J = cross
+
+    for hl in range(len(W)):
+        J += lamb * np.sum(np.power(W[hl], 2))
+
+    return J
+
+
+def compute_accuracy(X, y, W, b):
+    P = evaluate_classifier(X, W, b)[0]
+    preds = np.argmax(P, axis=0) #the predicted class for each picture
+    are_equal = np.sum(preds == y)
+    return are_equal/preds.shape[0]
+
+
+def compute_gradients(X, Y, P, H, W, lamb):
+    dldb = []
+    dldw = []
+
+    for i in range(len(W)):
+
+        # Initialize
+        dldb.append(np.zeros((W[i].shape[0], 1)))
+        dldw.append(np.zeros(W[i].shape))
+
+    g = -np.transpose(Y - P)
+
+    for i in reversed(range(len(W))):
+        gNew = np.zeros((X.shape[1], W[i-1].shape[0]))
+
+        if i > 0:
+            for ind in range(X.shape[1]):
+                gpart = g[ind, :].reshape(-1, 1)
+                hpart = H[i][:, ind].reshape(-1, 1)
+
+                dldb[i] += gpart
+
+                dldw[i] += np.matmul(gpart, hpart.T)
+
+                ind_fun = np.where(hpart > 0, 1, 0).reshape(-1, 1)
+
+                gTemp = np.matmul(gpart.T, W[i])
+                gNew[ind, :] = np.multiply(gTemp.T, ind_fun).T
+
+        else:
+            dldw[i] += np.matmul(g.T, H[i].T)
+
+        g = gNew.copy()
+
+        dldb[i] /= X.shape[1]
+        dldw[i] /= X.shape[1]
+
+        dldw[i] += (2 * lamb * W[i])
+
+    return dldb, dldw
+
+
+def disp_img(image):
+    plt.imshow(np.transpose(image.reshape(3, 32, 32), (1, 2, 0)))
+    plt.show()
+
+
+def compute_grads_num_slow(X, Y, W, b, lamb, h):
+
+    grad_b = []
+    grad_W = []
+
+    for i in range(len(W)):
+        # Initialize
+        grad_b.append(np.zeros(b[i].shape))
+        grad_W.append(np.zeros(W[i].shape))
+
+    for hl in range(len(W)): #for every hidden layer
+
+        for i in range(b[hl].shape[0]):
+            b_try = b[:]
+            b_try[hl][i] = b_try[hl][i] - h
+            c1 = compute_cost(X, Y, W, b_try, lamb)
+
+            b_try = b[:]
+            b_try[hl][i] = b_try[hl][i] + h
+            c2 = compute_cost(X, Y, W, b_try, lamb)
+
+            grad_b[hl][i] = ((c2 - c1) / (2 * h))
+
+        for i in range(W[hl].shape[0]):
+            print("W loop, i: ", i)
+            for j in range(W[hl].shape[1]):
+                W_try = W[:]
+                W_try[hl][i, j] = W_try[hl][i, j] - h
+                c1 = compute_cost(X, Y, W_try, b, lamb)
+
+                W_try = W[:]
+                W_try[hl][i, j] = W_try[hl][i, j] + h
+                c2 = compute_cost(X, Y, W_try, b, lamb)
+
+                grad_W[hl][i, j] = ((c2 - c1) / (2 * h))
+
+    return grad_b, grad_W
+
+
+def mini_batch_GD(X, X_val, Y, Y_val, n_batch, eta, n_epochs, W, b, lamb, rho, dr):
+
+    int_cost = compute_cost(X, Y, W, b, lamb)
+
+    J_tr = []
+    J_val = []
+
+    vb = []
+    vW = []
+
+    for i in range(len(W)):
+        # Initialize
+        vb.append(np.zeros((W[i].shape[0], 1)))
+        vW.append(np.zeros(W[i].shape))
+
+    for i in range(n_epochs):
+        for j in range(int(X.shape[1]/n_batch)):
+            j_start = j * n_batch
+            j_end = (j+1) * n_batch
+            Xbatch = X[:, j_start: j_end]
+            Ybatch = Y[:, j_start: j_end]
+
+            P, H = evaluate_classifier(Xbatch, W, b)
+
+            djdb, djdw = compute_gradients(Xbatch, Ybatch, P, H, W, lamb)
+
+            for hl in range(len(W)):  # for every hidden layer
+                vb[hl] = (vb[hl] * rho) + (eta * djdb[hl])
+                vW[hl] = (vW[hl] * rho) + (eta * djdw[hl])
+
+                b[hl] -= vb[hl]
+                W[hl] -= vW[hl]
+
+        J_tr.append(compute_cost(X, Y, W, b, lamb))
+        J_val.append(compute_cost(X_val, Y_val, W, b, lamb))
+
+        #print("TR Cost for epoch ", i, " is ", J_tr[i])
+        #print("VAL Cost for epoch ", i, " is ", J_val[i])
+        print("    Epoch ", i, " TR: ", J_tr[i], " VAL: ", J_val[i])
+
+        if J_tr[-1] > 3* int_cost:
+            print("J_tr[-1] > 3* int_cost")
+            break
+
+        eta *= dr
+
+    Wstar = W
+    bstar = b
+
+    epochs = [x + 1 for x in range(n_epochs)]
+    plt.plot(epochs, J_tr, label="Training")
+    plt.plot(epochs, J_val, label="Validation")
+    plt.legend()
+    plt.ylabel("Loss")
+    plt.xlabel("Epochs")
+    plt.show()
+
+    return Wstar, bstar
---
+++ L3/l3_main.py
@@ -0,0 +1,136 @@
+from L3.functions import *
+import random
+
+def main():
+    am_labels = 10
+    X_tr, Y_tr, y_tr = readfile("../Datasets/data_batch_1")
+#    X_tr1, Y_tr1, y_tr1 = readfile("../Datasets/data_batch_2")
+#    X_tr2, Y_tr2, y_tr2 = readfile("../Datasets/data_batch_3")
+#    X_tr3, Y_tr3, y_tr3 = readfile("../Datasets/data_batch_4")
+#    X_tr4, Y_tr4, y_tr4 = readfile("../Datasets/data_batch_5")
+#
+#    X_tr = np.concatenate((X_tr, X_tr1), axis=1)
+#    X_tr = np.concatenate((X_tr, X_tr2), axis=1)
+#    X_tr = np.concatenate((X_tr, X_tr3), axis=1)
+#    X_tr = np.concatenate((X_tr, X_tr4[:, :9000]), axis=1)
+#
+#    Y_tr = np.concatenate((Y_tr, Y_tr1), axis=1)
+#    Y_tr = np.concatenate((Y_tr, Y_tr2), axis=1)
+#    Y_tr = np.concatenate((Y_tr, Y_tr3), axis=1)
+#    Y_tr = np.concatenate((Y_tr, Y_tr4[:, :9000]), axis=1)
+#
+#    y_tr = np.concatenate((y_tr, y_tr1), axis=0)
+#    y_tr = np.concatenate((y_tr, y_tr2), axis=0)
+#    y_tr = np.concatenate((y_tr, y_tr3), axis=0)
+#    y_tr = np.concatenate((y_tr, y_tr4[:9000]), axis=0)
+#
+#    X_val = X_tr4[:, 9000:10000]
+#    Y_val = Y_tr4[:, 9000:10000]
+#    y_val = y_tr4[9000:10000]
+
+    X_val, Y_val, y_val = readfile("../Datasets/data_batch_2")
+
+    X_test, Y_test, y_test = readfile("../Datasets/test_batch")
+
+    LOAD = False
+
+    mean_x = np.mean(X_tr, axis=1)
+
+    mean_x = np.reshape(mean_x, (-1, 1))
+
+    X_tr -= mean_x
+    X_val -= mean_x
+    X_test -= mean_x
+
+    am_nodes = [50, 30]  # number of nodes for the hidden layers
+
+    dim_img = len(X_val)
+
+    W, b = get_parameters_he(dim_img, am_labels, am_nodes, 1337)
+
+#    P, H = evaluate_classifier(X_tr[:, 0:100], W, b)
+
+#    djdb, djdw = compute_gradients(X_tr[:, 0:100], Y_tr[:, 0:100], P, H, W, 0.001)
+
+#    if LOAD:
+#        djdb2 = load_mats("djdb2", "mats")
+#        djdw2 = load_mats("djdw2", "mats")
+#    else:
+#        djdb2, djdw2 = compute_grads_num_slow(X_tr[:, 0:100], Y_tr[:, 0:100], W, b, 0.001, 0.00001)
+#        save_mats(djdb2, "djdb2", "mats")
+#        save_mats(djdw2, "djdw2", "mats")
+#
+#    for lay in range(len(am_nodes) + 1):
+#        print("lay: " + str(lay))
+#        diff_b = djdb[lay] - djdb2[lay]
+#        diff_w = djdw[lay] - djdw2[lay]
+#
+#
+#        bsum = np.sum(np.abs(diff_b)) / b[lay].size
+#        wsum = np.sum(np.abs(diff_w)) / W[lay].size
+#
+#        print("bsum: ", bsum)
+#        print("wsum: ", wsum)
+#
+#
+#    acc_before_train = compute_accuracy(X_test, y_test, W, b)
+#    print("Accuracy before training: ", acc_before_train)
+
+
+### FINDING PAIRS
+
+#    n_batch = 100
+#    n_epochs = 20
+#    rho = 0.9
+#    dr = 0.95  # decay rate
+#
+#    eta_lower = 0.0055
+#    eta_upper = 0.008
+#
+#    lamb_lower = 0.0045
+#    lamb_upper = 0.006
+#
+#    pairing_tries = 100
+#
+#    results = np.zeros((pairing_tries, 3))
+#
+#    for t in range(pairing_tries):
+#
+#        eta = random.uniform(eta_lower, eta_upper)
+#        lamb = random.uniform(lamb_lower, lamb_upper)
+#
+#        Wstar, bstar = mini_batch_GD(X_tr, X_val, Y_tr, Y_val, n_batch, eta, n_epochs, W, b, lamb, rho, dr)
+#
+#        acc = compute_accuracy(X_test, y_test, Wstar, bstar)
+#
+#        print("pair: " + str(t) + " acc: " + str(acc), " eta: " + str(eta) + " lamb: " + str(lamb))
+#
+#        results[t, :] = [acc, eta, lamb]
+#
+#        # Sort results based on descending accuracy
+#        results = results[results[:, 0].argsort()[::-1]]
+#
+#        np.savetxt("results.txt", results, fmt="%1.5f")
+
+
+
+### FINAL NETWORK STUFF
+
+    n_batch = 10
+    n_epochs = 150
+    rho = 0.9
+    dr = 0.95  # decay rate
+    eta = 0.01
+    lamb = 0
+
+
+    Wstar, bstar = mini_batch_GD(X_tr[:, 0:100], X_val[:, 0:100], Y_tr[:, 0:100], Y_val[:, 0:100], n_batch, eta, n_epochs, W, b, lamb, rho, dr)
+
+    acc = compute_accuracy(X_test, y_test, Wstar, bstar)
+
+    print("Accuracy after training: ", acc)
+
+
+
+if __name__ == "__main__":
+    main()
